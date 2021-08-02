6f4c014eeccb68133f8ece9670d6c17ab2b63c53
Denis Dolgopolov <jlomuk1990@gmail.com>
2021-08-02 19:51:26 +0300

    first module test, add fake views, resolve url

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Установка прошла' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """тест нового посетителя"""
+
+    def setUp(self) -> None:
+        """Начальная конфигурация"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        """Деструктор"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Можно начать новый список и получить его позже"""
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('FINISH TEST')
+
+
+if __name__ == '__main__':
+    unittest.main()
