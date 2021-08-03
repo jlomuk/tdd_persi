2fe96e0a205db119d8fc3c830ccf3f4552435622
Denis Dolgopolov <jlomuk1990@gmail.com>
2021-08-03 17:59:49 +0300

    updated func test. The test is check posible input a text

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
+import time
 import unittest
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,7 +20,19 @@ class NewVisitorTest(unittest.TestCase):
         """Можно начать новый список и получить его позже"""
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        self.fail('FINISH TEST')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1 : Купить павлиньи перья' for row in rows)
+        )
 
 
 if __name__ == '__main__':
