e87261e41032633e2b83545e1a8ab45310936195
Denis Dolgopolov <jlomuk1990@gmail.com>
2021-08-04 19:37:36 +0300

    refactor module tests

--- functional_tests.py
+++ functional_tests.py
@@ -16,27 +16,30 @@ class NewVisitorTest(unittest.TestCase):
         """Деструктор"""
         self.browser.quit()
 
+    def _check_for_row_in_list_table(self, row_text):
+        """Проверка нахождения строки  таблице шаблона"""
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def _input_new_item_in_table(self, item):
+        """Ввод новых элементов в шаблон"""
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(item)
+        inputbox.send_keys(Keys.ENTER)
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Можно начать новый список и получить его позже"""
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do lists', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-        inputbox.send_keys('Купить павлиньи перья')
-        inputbox.send_keys(Keys.ENTER)
+        self._input_new_item_in_table('Купить павлиньи перья')
         time.sleep(2)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Сделать мушку из павлиньих перьев')
-        inputbox.send_keys(Keys.ENTER)
+        self._input_new_item_in_table('Сделать мушку из павлиньих перьев')
         time.sleep(2)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1 : Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn('2 : Сделать мушку из павлиньих перьев', [row.text for row in rows])
-
+        self._check_for_row_in_list_table('1: Купить павлиньи перья')
+        self._check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
 
 if __name__ == '__main__':
