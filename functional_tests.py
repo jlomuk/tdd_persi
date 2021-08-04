d725d4afa059d787630a715b274f9425f376fc95
Denis Dolgopolov <jlomuk1990@gmail.com>
2021-08-04 19:15:12 +0300

    add new test for post request

--- functional_tests.py
+++ functional_tests.py
@@ -26,13 +26,17 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Купить павлиньи перья')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1 : Купить павлиньи перья' for row in rows)
-        )
+        self.assertIn('1 : Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn('2 : Сделать мушку из павлиньих перьев', [row.text for row in rows])
+
 
 
 if __name__ == '__main__':
