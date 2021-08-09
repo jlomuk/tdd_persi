40e31cf04cfda1bf4494aac5d8cf34ab5c7d9684
Денис Долгополов <dolgopolov@ssmp.local>
2021-08-09 11:00:05 +0300

    update func test: add check different lists

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -3,6 +3,22 @@ import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
+
+def wait_element(func):
+    MAX_WAIT = 5
+    def wraper(*args, **kwargs):
+        start = time.time()
+        while True:
+            try:
+                func(*args, *kwargs)
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return wraper
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -19,6 +35,7 @@ class NewVisitorTest(LiveServerTestCase):
         """Деструктор"""
         self.browser.quit()
 
+    @wait_element
     def _check_for_row_in_list_table(self, row_text):
         """Проверка нахождения строки  таблице шаблона"""
         table = self.browser.find_element_by_id('id_list_table')
@@ -31,16 +48,34 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(item)
         inputbox.send_keys(Keys.ENTER)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         """Можно начать новый список и получить его позже"""
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do lists', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         self._input_new_item_in_table('Купить павлиньи перья')
-        time.sleep(2)
         self._input_new_item_in_table('Сделать мушку из павлиньих перьев')
-        time.sleep(2)
         self._check_for_row_in_list_table('1: Купить павлиньи перья')
         self._check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        self._input_new_item_in_table('Купить павлиньи перья')
+        self._check_for_row_in_list_table('1: Купить павлиньи перья')
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.tearDown()
+        self.setUp()
+        self._input_new_item_in_table('Сделать мушку из павлиньих перьев')
+        self._check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertIn('Сделать мушку из павлиньих перьев', page_text)
+
+
+
 
