503fa0ce1a9c09e0e44d6092876d244acffd7617
Денис Долгополов <dolgopolov@ssmp.local>
2021-08-10 09:16:49 +0300

    refactoring urls

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from lists import views
+from lists import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name='home_page'),
-    path('lists/new/', views.new_list, name='new_list'),
-    path('lists/<int:id>/add_item/', views.add_item, name='add_item'),
-    path('lists/<int:id>/', views.view_list, name='view_list'),
+    path('lists/', include('lists.urls')),
 
 ]
