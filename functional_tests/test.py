5170fe6b1f2962e113a67819da974359c13d37d8
Денис Долгополов <dolgopolov@ssmp.local>
2021-08-10 09:56:16 +0300

    add new functional test

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -77,6 +77,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Купить павлиньи перья', page_text)
         self.assertIn('Сделать мушку из павлиньих перьев', page_text)
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
 
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        _check_for_row_in_list_table('1: testing')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+        
