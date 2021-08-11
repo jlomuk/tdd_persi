6b57c757aafbcc7112bea95a4e286f4fb8150e70
Денис Долгополов <dolgopolov@ssmp.local>
2021-08-11 10:36:56 +0300

    add new func tests check post request with empty value

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -23,10 +23,9 @@ class FunctionalTest(StaticLiveServerTestCase):
         """Деструктор"""
         self.browser.quit()
 
-    def wait_element(func):
-        """Декоратор для ожидания"""
+    def _wait_element(func):
+        """Декоратор для ожидания отображения элемента"""
         MAX_WAIT = 5
-
         def wraper(*args, **kwargs):
             start = time.time()
             while True:
@@ -39,7 +38,7 @@ class FunctionalTest(StaticLiveServerTestCase):
                     time.sleep(0.5)
         return wraper
 
-    @wait_element
+    @_wait_element
     def _check_for_row_in_list_table(self, row_text):
         """Проверка нахождения строки  таблице шаблона"""
         table = self.browser.find_element_by_id('id_list_table')
@@ -53,70 +52,3 @@ class FunctionalTest(StaticLiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
 
-class NewVisitorTest(FunctionalTest):
-    """тест нового посетителя"""
-
-    def test_can_start_a_list_for_one_user(self):
-        """Можно начать новый список и получить его позже"""
-        self.browser.get(self.live_server_url)
-        self.assertIn('To-Do lists', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-        self._input_new_item_in_table('Купить павлиньи перья')
-        self._check_for_row_in_list_table('1: Купить павлиньи перья')
-        self._input_new_item_in_table('Сделать мушку из павлиньих перьев')
-        self._check_for_row_in_list_table(
-            '2: Сделать мушку из павлиньих перьев')
-
-    def test_multiple_users_can_start_lists_at_different_urls(self):
-        self.browser.get(self.live_server_url)
-        self._input_new_item_in_table('Купить павлиньи перья')
-        self._check_for_row_in_list_table('1: Купить павлиньи перья')
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
-        self.browser.quit()
-        self.browser = webdriver.Chrome()
-        self.browser.get(self.live_server_url)
-        self._input_new_item_in_table('Сделать мушку из павлиньих перьев')
-        self._check_for_row_in_list_table(
-            '1: Сделать мушку из павлиньих перьев')
-        francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/lists/.+')
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotEqual(francis_list_url, edith_list_url)
-        self.assertNotIn('Купить павлиньи перья', page_text)
-        self.assertIn('Сделать мушку из павлиньих перьев', page_text)
-
-
-class LayoutAndStylingTest(FunctionalTest):
-
-    def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self._check_for_row_in_list_table('1: testing')
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-
-class ItemValidationTest(FunctionalTest):
-
-    @skip
-    def test_cannot_add_empty_list_items(self):
-        pass
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,10 +1,17 @@
 from unittest import skip
+from selenium.webdriver.common.keys import Keys
 
 from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
 
-    @skip
     def test_cannot_add_empty_list_items(self):
-        pass
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        wait_css_element = FunctionalTest._wait_element(
+            self.browser.find_element_by_css_selector)
+        self.assertEqual(
+            wait_css_element('.has-error').text,
+            "You can't have an empty list item"
+        )
