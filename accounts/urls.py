2bf2f62f788f63eb70477db25e4b15bda3a4d12f
Денис Долгополов <dolgopolov@ssmp.local>
2021-08-16 09:48:34 +0300

    add new view send email messages, added test for view

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from accounts import views
 
 urlpatterns = [
     path('send_login_email/', views.send_login_email, name='send_login_email'),
+    path('login', views.login_view, name='login'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,27 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.urls import reverse
+
+from .models import Token
 
 
 def send_login_email(request):
     email = request.POST.get('email')
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n {url}'
     send_mail(
         'Your login link for Superlists',
-        'body text',
+        message_body,
         'noreply@superlists',
         [email]
     )
+    messages.success(request, 'Check your email, we send link for enter site')
+    return redirect('/')
+
+
+def login_view(request):
     return redirect('/')
